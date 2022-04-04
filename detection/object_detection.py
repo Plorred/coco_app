536026fd46efd0dd39105806c10f3c753100361f
Timur Sarov <timursarov19@gmail.com>
2022-04-04 23:53:58 +0300

    huge updates

--- detection/object_detection.py
+++ detection/object_detection.py
@@ -1,14 +1,15 @@
+import itertools as it
 import os
+import random
 
 import cv2
 import numpy as np
 import streamlit as st
 from matplotlib import colors
-import random
-import itertools as it
 
 colors: list = [c for c in it.product([0, 128, 255], repeat=3)]
-colors = sorted(colors, key = lambda x: sum(x))
+colors = sorted(colors, key=lambda x: sum(x))
+
 
 def detect_object(frame, req_class, precision):
     cfg_path: str = os.path.abspath("yolo/yolov4.cfg")
@@ -31,6 +32,9 @@ def detect_object(frame, req_class, precision):
 
     classes, confidences, boxes = net.detect(frame, confThreshold=0.1, nmsThreshold=0.4)
     final_color = random.choice(colors)
+    text_color = (0, 0, 0)
+    if final_color == (0, 0, 0):
+        text_color = (255, 255, 255)
     for classId, confidence, box in zip(
         classes.flatten(), confidences.flatten(), boxes
     ):
@@ -38,6 +42,27 @@ def detect_object(frame, req_class, precision):
         label = "%.2f" % confidence
         label = "%s" % (names[classId])
         label = "%s: %.2f" % (names[classId], confidence)
+        if req_class == "anything" and confidence >= precision:
+            labelSize, baseLine = cv2.getTextSize(
+                label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1
+            )  # fontScale: 0.5, thickness: 1
+            # print(labelSize, baseLine)
+            left, top, width, height = box
+            # print("T:", top)
+            top = max(top, labelSize[1])
+            # print("MT:", top)
+            cv2.rectangle(frame, box, color=final_color, thickness=3)
+            # Draw rectangle for labels
+            cv2.rectangle(
+                frame,
+                (left - 2, top - labelSize[1] - 5),
+                (left + labelSize[0], top - 1),
+                final_color,
+                cv2.FILLED,
+            )
+            cv2.putText(
+                frame, label, (left, top - 5), cv2.FONT_HERSHEY_DUPLEX, 0.5, text_color
+            )
         if names[classId] != req_class:
             continue
         elif confidence < precision:
@@ -61,7 +86,7 @@ def detect_object(frame, req_class, precision):
                 cv2.FILLED,
             )
             cv2.putText(
-                frame, label, (left, top - 5), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0,0,0)
+                frame, label, (left, top - 5), cv2.FONT_HERSHEY_DUPLEX, 0.5, text_color
             )
 
     return frame
--- myselfdrive.py
+++ myselfdrive.py
@@ -2,6 +2,7 @@ import glob
 import os
 import os.path as Path
 import urllib
+from cProfile import run
 from curses import meta
 
 import altair as alt
@@ -13,89 +14,9 @@ from PIL import Image
 
 from detection.object_detection import detect_object
 
-images: str = glob.glob(Path.join("data/test", "*.jpg"))
-titles: dict = {
-    1: u"person",
-    2: u"bicycle",
-    3: u"car",
-    4: u"motorcycle",
-    5: u"airplane",
-    6: u"bus",
-    7: u"train",
-    8: u"truck",
-    9: u"boat",
-    10: u"traffic light",
-    11: u"fire hydrant",
-    12: u"stop sign",
-    13: u"parking meter",
-    14: u"bench",
-    15: u"bird",
-    16: u"cat",
-    17: u"dog",
-    18: u"horse",
-    19: u"sheep",
-    20: u"cow",
-    21: u"elephant",
-    22: u"bear",
-    23: u"zebra",
-    24: u"giraffe",
-    25: u"backpack",
-    26: u"umbrella",
-    27: u"handbag",
-    28: u"tie",
-    29: u"suitcase",
-    30: u"frisbee",
-    31: u"skis",
-    32: u"snowboard",
-    33: u"sports ball",
-    34: u"kite",
-    35: u"baseball bat",
-    36: u"baseball glove",
-    37: u"skateboard",
-    38: u"surfboard",
-    39: u"tennis racket",
-    40: u"bottle",
-    41: u"wine glass",
-    42: u"cup",
-    43: u"fork",
-    44: u"knife",
-    45: u"spoon",
-    46: u"bowl",
-    47: u"banana",
-    48: u"apple",
-    49: u"sandwich",
-    50: u"orange",
-    51: u"broccoli",
-    52: u"carrot",
-    53: u"hot dog",
-    54: u"pizza",
-    55: u"donut",
-    56: u"cake",
-    57: u"chair",
-    58: u"couch",
-    59: u"potted plant",
-    60: u"bed",
-    61: u"dining table",
-    62: u"toilet",
-    63: u"tv",
-    64: u"laptop",
-    65: u"mouse",
-    66: u"remote",
-    67: u"keyboard",
-    68: u"cell phone",
-    69: u"microwave",
-    70: u"oven",
-    71: u"toaster",
-    72: u"sink",
-    73: u"refrigerator",
-    74: u"book",
-    75: u"clock",
-    76: u"vase",
-    77: u"scissors",
-    78: u"teddy bear",
-    79: u"hair drier",
-    80: u"toothbrush",
-}
+with open("yolo/coco.names") as file:
+    lines = [line.rstrip() for line in file]
+lines.insert(0, "anything")
 
 
 def main():
@@ -106,6 +27,7 @@ def main():
     )
     if app_mode == "О программе":
         st.sidebar.success('Чтобы продолжить, выберите "Запуск приложения"')
+        st.image("readme.png")
     elif app_mode == "Исходный код":
         readme_text.empty()
         st.title("Код главного файла")
@@ -114,12 +36,21 @@ def main():
         st.code(open("detection/object_detection.py").read())
     elif app_mode == "Запуск приложения":
         readme_text.empty()
-        run_the_app()
+        image_pack = st.sidebar.selectbox(
+            "Выберите набор картинок", ["Транспорты и пешеходы", "Случайные картинки"]
+        )
+        if image_pack == "Транспорты и пешеходы":
+            DATA_ROOT: str = "data/test"
+            run_the_app(DATA_ROOT)
+        elif image_pack == "Случайные картинки":
+            DATA_ROOT: str = "data/test2"
+            run_the_app(DATA_ROOT)
 
 
-def run_the_app():
+def run_the_app(data_root: str):
+    DATA_ROOT = data_root
     selected_frame_index, selected_frame = frame_selector_ui(DATA_ROOT)
-    orig_img = load_image(selected_frame_index)
+    orig_img = load_image(selected_frame_index, DATA_ROOT)
     orig_img = cv2.resize(orig_img, dsize=(704, 704), interpolation=cv2.INTER_AREA)
     image = st.image(orig_img)
     image.empty()
@@ -128,12 +59,13 @@ def run_the_app():
 
 
 def object_selector(img, precision: int):
-    req_class = st.sidebar.selectbox("Выберите объект", list(titles.values()))
+    req_class = st.sidebar.selectbox("Выберите объект", lines)
     image = detect_object(img, req_class, precision)
     return st.image(image)
 
 
 def frame_selector_ui(data_root: str):
+    images: str = glob.glob(Path.join(data_root, "*.jpg"))
     selected_frame_index = st.sidebar.slider("Выберите картинку", 0, 256)
     selected_frame = images[selected_frame_index]
     return selected_frame_index, selected_frame
@@ -145,7 +77,8 @@ def precision_value():
 
 
 @st.cache(show_spinner=False)
-def load_image(img_idx: int):
+def load_image(img_idx: int, data_root: str):
+    images: str = glob.glob(Path.join(data_root, "*.jpg"))
     image = images[img_idx]
     if image is not None:
         try:
@@ -157,8 +90,6 @@ def load_image(img_idx: int):
             return img_array
     return image
 
-
-DATA_ROOT: str = "data/test"
 
 if __name__ == "__main__":
     main()
