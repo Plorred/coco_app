9e643caf02e6be2ba5c2c07633ac5fa7412e2c7c
Timur Sarov <timursarov19@gmail.com>
2022-04-05 14:53:16 +0300

    added paths parsing compatibility

--- detection/object_detection.py
+++ detection/object_detection.py
@@ -1,32 +1,24 @@
 import itertools as it
-import os
 import random
 
 import cv2
-import numpy as np
-import streamlit as st
-from matplotlib import colors
 
 colors: list = [c for c in it.product([0, 128, 255], repeat=3)]
 colors = sorted(colors, key=lambda x: sum(x))
 
 
-def detect_object(frame, req_class, precision):
-    cfg_path: str = os.path.abspath("yolo/yolov4.cfg")
-    weights_path: str = os.path.abspath("yolo/yolov4.weights")
-    names_path: str = os.path.abspath("yolo/coco.names")
-
-    # Load Yolo
-    # net = cv2.dnn_DetectionModel('yolov4.cfg', 'yolov4.weights')
+def detect_object(frame, req_class, precision, args):
+    cfg_path: str = args.cfg
+    weights_path: str = args.weights
+    names_path: str = args.names
+    
     net = cv2.dnn_DetectionModel(cfg_path, weights_path)
     net.setInputSize(704, 704)
     net.setInputScale(1.0 / 255)
     net.setInputSwapRB(True)
 
-    # Resize the image
     frame = cv2.resize(frame, dsize=(704, 704), interpolation=cv2.INTER_AREA)
 
-    # with open('coco.names', 'rt') as f:
     with open(names_path, "rt") as f:
         names: list = f.read().rstrip("\n").split("\n")
 
@@ -38,21 +30,17 @@ def detect_object(frame, req_class, precision):
     for classId, confidence, box in zip(
         classes.flatten(), confidences.flatten(), boxes
     ):
-        # print(classId, confidence, box)
         label = "%.2f" % confidence
         label = "%s" % (names[classId])
         label = "%s: %.2f" % (names[classId], confidence)
         if req_class == "anything" and confidence >= precision:
             labelSize, baseLine = cv2.getTextSize(
                 label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1
-            )  # fontScale: 0.5, thickness: 1
-            # print(labelSize, baseLine)
+            ) 
+          
             left, top, width, height = box
-            # print("T:", top)
             top = max(top, labelSize[1])
-            # print("MT:", top)
             cv2.rectangle(frame, box, color=final_color, thickness=3)
-            # Draw rectangle for labels
             cv2.rectangle(
                 frame,
                 (left - 2, top - labelSize[1] - 5),
@@ -70,14 +58,12 @@ def detect_object(frame, req_class, precision):
         else:
             labelSize, baseLine = cv2.getTextSize(
                 label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1
-            )  # fontScale: 0.5, thickness: 1
-            # print(labelSize, baseLine)
+            ) 
+ 
             left, top, width, height = box
-            # print("T:", top)
+          
             top = max(top, labelSize[1])
-            # print("MT:", top)
             cv2.rectangle(frame, box, color=final_color, thickness=2)
-            # Draw rectangle for labels
             cv2.rectangle(
                 frame,
                 (left - 2, top - labelSize[1] - 5),
